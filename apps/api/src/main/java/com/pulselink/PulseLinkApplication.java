60f037157653e5eb0ad52a074d7030235d8342e7
NguyenTruongNgocHan <23520434@gm.uit.edu.vn>
2026-06-18 00:19:54 +0700

    chore: initialize PulseLink monorepo

---
+++ apps/api/src/main/java/com/pulselink/PulseLinkApplication.java
@@ -0,0 +1,14 @@
+package com.pulselink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(
+            PulseLinkApplication.class,
+            args
+        );
+    }
+}
